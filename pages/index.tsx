0a52dc0e5873724c11ea35f00b7f8a4381ed3b2f
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-03-29 21:55:03 +0900

    DynamicImport追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,17 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
 
 const index = () => {
-  const now = new Date();
-  const [hour, setHour] = useState(now.getHours());
-  const [min, setMin] = useState(now.getMinutes());
-  const [sec, setSec] = useState(now.getSeconds());
-  setInterval(() => {
-    const now = new Date();
-    setHour(now.getHours());
-    setMin(now.getMinutes());
-    setSec(now.getSeconds());
-  }, 1000);
+  const DynamicComponentWithNoSSR = dynamic(() => import('../components/lv1/Timer'), {
+    ssr: false,
+  });
 
   return (
     <>
       <Head>
         <title>Timer</title>
       </Head>
-      <div>Timer</div>
-      <div>
-        {hour}時{min}分{sec}秒
-      </div>
+      <DynamicComponentWithNoSSR />
     </>
   );
 };
