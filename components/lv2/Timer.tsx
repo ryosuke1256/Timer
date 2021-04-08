cd28b02ad084119843b08e8d232245e14d86ce9c
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-04-08 14:01:51 +0900

    追加

--- components/lv2/Timer.tsx
+++ components/lv2/Timer.tsx
@@ -8,21 +8,22 @@ const Timer: React.VFC = () => {
   const log = () => {
     console.log('a');
     setWord('３秒経過しました');
-    function sound(type, sec) {
-      console.log('soundが鳴りました');
-      const ctx = new AudioContext();
-      const osc = ctx.createOscillator();
-      osc.type = type;
-      osc.connect(ctx.destination);
-      osc.start();
-      osc.stop(sec);
-    }
-
-    sound('sign', 0.3);
+    sound('sign', 1);
   };
+
   const click: VoidFunction = () => {
     console.log(click);
     setTimeout(log, 3000);
+  };
+
+  const sound = (type, sec) => {
+    console.log('soundが鳴りました');
+    const ctx = new AudioContext();
+    const osc = ctx.createOscillator();
+    osc.type = type;
+    osc.connect(ctx.destination);
+    osc.start();
+    osc.stop(sec);
   };
 
   return (
