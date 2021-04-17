1b4f09e3370b0350eec2f2be2726b84135976c78
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-04-17 12:30:52 +0900

    修正

---
+++ components/lv1/NavClock.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NavClock = () => {
+  return <div></div>;
+};
+
+export default NavClock;
---
+++ components/lv1/NavTimer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Image from 'next/image';
+
+const NavTimer = () => {
+  return <Image src="/images/clock512.png" alt="clock" width="64" height="64" />;
+};
+
+export default NavTimer;
